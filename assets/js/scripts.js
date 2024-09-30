1782841622b2c0a8829920853a45888c702bd59d
Daniel Bautista P. <76835848+Danielbp019@users.noreply.github.com>
2024-09-30 01:45:38 -0500

    Update, estilo index
    index estilo de dos columnas y botón switch día y noche

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,31 @@
 // Cambio dia y noche script
-document.querySelectorAll('[data-bs-theme-value]').forEach(button => {
-    button.addEventListener('click', () => {
-        const theme = button.getAttribute('data-bs-theme-value');
-        document.documentElement.setAttribute('data-bs-theme', theme);
-        localStorage.setItem('theme', theme);
-    });
-});
-const savedTheme = localStorage.getItem('theme') || 'auto';
+// *    Inicializar el tema guardado
+const savedTheme = localStorage.getItem('theme') || 'light';
 document.documentElement.setAttribute('data-bs-theme', savedTheme);
+document.getElementById('themeSwitch').checked = savedTheme === 'dark';
+
+// *    Cambiar el tema al activar el switch
+document.getElementById('themeSwitch').addEventListener('change', function () {
+    const theme = this.checked ? 'dark' : 'light';
+    document.documentElement.setAttribute('data-bs-theme', theme);
+    localStorage.setItem('theme', theme);
+    updateThemeIcon(theme);
+});
+
+// *    Actualizar el icono SVG según el tema
+function updateThemeIcon(theme) {
+    const icon = document.querySelector('.theme-icon-active use');
+    if (theme === 'dark') {
+        icon.setAttribute('href', '#moon-stars-fill');
+        document.querySelector('.theme-icon-active').classList.add('theme-dark');
+    } else {
+        icon.setAttribute('href', '#sun-fill');
+        document.querySelector('.theme-icon-active').classList.remove('theme-dark');
+    }
+}
+// *    Llamar a la función para actualizar el icono al cargar la página
+updateThemeIcon(savedTheme);
+
 
 // Generar opciones para el select de Uso Diario (horas)
 const usoDiarioSelect = document.getElementById('usoDiario');
@@ -49,7 +67,7 @@ function calcularGasto() {
 
     // Agregar el cálculo a la lista y mantener solo los últimos 10
     calculos.unshift(calculo);
-    if (calculos.length > 10) {
+    if (calculos.length > 9) {
         calculos.pop();
     }
     // Actualizar la tabla del historial
